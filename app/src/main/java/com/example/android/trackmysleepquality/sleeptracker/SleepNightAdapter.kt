ce863f1b9137cc2f548b041e8523337ec17e0088
gr3y6h0st <jayvy19@gmail.com>
2020-07-07 21:34:18 -0700

    replaced ScrollView with RecyclerView in SleepTrackerFragment. Created SleepNightAdapter and started implementing RecylerView.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -15,3 +15,25 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
+    var data = listOf<SleepNight>()
+
+    //tells RecyclerView How many items there are
+    override fun getItemCount() = data.size
+
+    //show items at viewHolder positions passed
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
+}
