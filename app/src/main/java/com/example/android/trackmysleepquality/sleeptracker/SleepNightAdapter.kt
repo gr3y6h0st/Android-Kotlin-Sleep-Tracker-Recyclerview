3d90d8277beaa716b4780f30c4533b4e5eba593d
gr3y6h0st <jayvy19@gmail.com>
2020-07-08 23:44:11 -0700

    hooked up SleepNightAdapter to SleepTrackerFragment.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,15 +16,21 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
 
 class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
     var data = listOf<SleepNight>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
-    //tells RecyclerView How many items there are
     override fun getItemCount() = data.size
 
     //show items at viewHolder positions passed
@@ -34,6 +40,9 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater
+                .inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
     }
 }
